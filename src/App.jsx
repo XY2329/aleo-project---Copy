18018c8bf7d2c9f47699ea6d9a6b4fa33dbb802d
XY2329 <seexy1127@gmail.com>
2023-10-14 22:58:45 +0800

    eth

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useMemo } from "react";
+import { WalletProvider } from "@demox-labs/aleo-wallet-adapter-react";
+import { WalletModalProvider } from "@demox-labs/aleo-wallet-adapter-reactui";
+import { LeoWalletAdapter } from "@demox-labs/aleo-wallet-adapter-leo";
+import {
+  DecryptPermission,
+  WalletAdapterNetwork,
+} from "@demox-labs/aleo-wallet-adapter-base";
+import { WalletMultiButton } from "@demox-labs/aleo-wallet-adapter-reactui";
+
+// Default styles that can be overridden by your app
+import("@demox-labs/aleo-wallet-adapter-reactui/styles.css");
+
+const App = () => {
+  const wallets = useMemo(
+    () => [
+      new LeoWalletAdapter({
+        appName: "Leo Demo App",
+      }),
+    ],
+    []
+  );
+
+  return (
+    <WalletProvider
+      wallets={wallets}
+      decryptPermission={DecryptPermission.UponRequest}
+      network={WalletAdapterNetwork.Localnet}
+      autoConnect
+    >
+      <WalletModalProvider>
+            <WalletMultiButton />
+      </WalletModalProvider>
+    </WalletProvider>
+  );
+};
+
+export default App;
